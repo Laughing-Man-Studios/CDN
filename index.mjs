c4ea32e852ac29ab463de962881c1c085df7dc54
Robert Gibb <rogibb111@gmail.com>
2023-05-08 15:35:35 -0600

    Updating where port is defind

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import express from 'express'
 import path from 'path';
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve('demo/index.html'));
