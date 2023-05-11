308298e7712bebb4dc94d5097f2071a7a88589b9
Robert Gibb <rogibb111@gmail.com>
2023-05-11 07:32:55 -0600

    Adding lit-graph to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,17 @@
-export default {
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+export default [{
     input: 'demo/main.js',
     output: {
         file: 'dist/demo.min.js',
-        format: 'cjs'
-    }
-}
+    },
+    plugins: [nodeResolve()]
+},
+{
+    input: 'node_modules/lit-graph/lit-graph.js',
+    output: {
+        file: 'dist/lit-graph.min.js',
+    },
+    context: 'window',
+    plugins: [nodeResolve()]  
+}]
