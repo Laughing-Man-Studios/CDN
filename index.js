00b4b27b33d6bb7afd01730a7781d58dc8b73e24
Robert Gibb <rogibb111@gmail.com>
2023-07-23 10:15:45 -0600

    Adding Cors plugin to express and setting options

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import path from 'path';
+import cors from 'cors';
+
 const app = express();
 const port = process.env.PORT || 3000;
+const origin = process.env.NODE_ENV === 'production' ?
+  'http://ski-resort-dashboard.laughing-man-studios.us.to' : 'http://localhost:3000'
+const corsOptions = { origin };
 
+app.use(cors(corsOptions));
 app.get('/', (req, res) => {
   res.sendFile(path.resolve('demo/index.html'));
 })
